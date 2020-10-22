5cd429496567f4077de7b586486af830e168b602
Robert Swain <robert.swain@gmail.com>
2020-10-22 16:13:13 +0200

    Tracy

--- src/trace.rs
+++ src/trace.rs
@@ -1,19 +1,17 @@
-use tracing_flame::FlameLayer;
 use tracing_subscriber::{fmt, prelude::*, registry::Registry, EnvFilter};
+use tracing_tracy::TracyLayer;
 
-pub fn setup_global_subscriber() -> impl Drop {
+pub fn setup_global_subscriber() {
     let fmt_layer = fmt::Layer::default();
     let filter_layer = EnvFilter::try_from_default_env()
         .or_else(|_| EnvFilter::try_new("info,wgpu=warn"))
         .unwrap();
-
-    let (flame_layer, _guard) = FlameLayer::with_file("./tracing.folded").unwrap();
+    let tracy_layer = TracyLayer::new();
 
     let subscriber = Registry::default()
         .with(filter_layer)
         .with(fmt_layer)
-        .with(flame_layer);
+        .with(tracy_layer);
 
     tracing::subscriber::set_global_default(subscriber).expect("Could not set global default");
-    _guard
 }
