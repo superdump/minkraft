435fd412d8be4f628d91c2bcea4df31105cbdcc2
Robert Swain <robert.swain@gmail.com>
2021-04-17 20:35:15 +0200

    Fix copyright and license statements

--- src/level_of_detail.rs
+++ src/level_of_detail.rs
@@ -2,6 +2,7 @@
  * MIT License
  *
  * Copyright (c) 2020 bonsairobo
+ * Copyright (c) 2021 Robert Swain <robert.swain@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
