bf14e743ff48de57dd14cc491b7cc0b119bd8e21
Robert Swain <robert.swain@gmail.com>
2021-04-23 11:54:40 +0200

    Fix collisions - lod center was way off

--- src/level_of_detail.rs
+++ src/level_of_detail.rs
@@ -55,13 +55,11 @@ pub fn level_of_detail_system(
     mut lod_state: ResMut<LodState>,
     mut mesh_commands: ResMut<MeshCommandQueue>,
 ) {
-    let mut camera_position = if let Some((_camera, tfm)) = cameras.iter().next() {
+    let camera_position = if let Some((_camera, tfm)) = cameras.iter().next() {
         tfm.translation
     } else {
         return;
     };
-    // TODO: Remove this when no longer debugging
-    camera_position.y = 0.0f32;
 
     let lod0_center = Point3f::from(camera_position).in_voxel() >> voxel_map_config.chunk_log2;
 
