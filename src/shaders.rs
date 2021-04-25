4e06767660cf00ea595d297007b5f5642d1903a4
Robert Swain <robert.swain@gmail.com>
2021-04-25 11:10:54 +0200

    Move shaders into assets subdirectory

--- src/shaders.rs
+++ src/shaders.rs
@@ -1,2 +1,3 @@
-pub const VERTEX_SHADER: &str = include_str!("array_texture.vert");
-pub const FRAGMENT_SHADER: &str = include_str!("array_texture.frag");
+pub const ARRAY_TEXTURE_VERTEX_SHADER: &str = include_str!("../assets/shaders/array_texture.vert");
+pub const ARRAY_TEXTURE_FRAGMENT_SHADER: &str =
+    include_str!("../assets/shaders/array_texture.frag");
