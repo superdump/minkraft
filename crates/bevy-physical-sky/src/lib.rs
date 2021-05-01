e579a378f151d117b6e2be6e969f2df796b085cb
Robert Swain <robert.swain@gmail.com>
2021-05-01 18:23:55 +0200

    bevy-physical-sky: Add preset constructors

--- crates/bevy-physical-sky/src/lib.rs
+++ crates/bevy-physical-sky/src/lib.rs
@@ -110,6 +110,150 @@ impl PhysicalSkyMaterial {
         self.sun_position.y = distance * azimuth.sin() * inclination.sin();
         self.sun_position.z = distance * azimuth.sin() * inclination.cos();
     }
+
+    pub fn stellar_dawn(update_sun_position: bool) -> Self {
+        Self {
+            mie_k_coefficient: Vec4::new(0.686, 0.678, 0.666, 0.0),
+            primaries: Vec4::new(6.8e-7, 5.5e-7, 4.5e-7, 0.0),
+            depolarization_factor: 0.067,
+            luminance: 1.0,
+            mie_coefficient: 0.00335,
+            mie_directional_g: 0.787,
+            mie_v: 4.012,
+            mie_zenith_length: 500.0,
+            num_molecules: 2.542e25,
+            rayleigh_zenith_length: 615.0,
+            rayleigh: 1.00,
+            refractive_index: 1.000317,
+            sun_angular_diameter_degrees: 0.00758,
+            sun_intensity_factor: 1111.0,
+            sun_intensity_falloff_steepness: 0.98,
+            tonemap_weighting: 9.50,
+            turbidity: 1.25,
+            update_sun_position,
+            ..Default::default()
+        }
+    }
+
+    pub fn red_sunset(update_sun_position: bool) -> Self {
+        Self {
+            mie_k_coefficient: Vec4::new(0.686, 0.678, 0.666, 0.0),
+            primaries: Vec4::new(6.8e-7, 5.5e-7, 4.5e-7, 0.0),
+            turbidity: 4.7,
+            rayleigh: 2.28,
+            mie_coefficient: 0.005,
+            mie_directional_g: 0.82,
+            luminance: 1.00,
+            refractive_index: 1.00029,
+            num_molecules: 2.542e25,
+            depolarization_factor: 0.02,
+            rayleigh_zenith_length: 8400.0,
+            mie_v: 3.936,
+            mie_zenith_length: 34000.0,
+            sun_intensity_factor: 1000.0,
+            sun_intensity_falloff_steepness: 1.5,
+            sun_angular_diameter_degrees: 0.00933,
+            tonemap_weighting: 9.50,
+            update_sun_position,
+            ..Default::default()
+        }
+    }
+
+    pub fn alien_day(update_sun_position: bool) -> Self {
+        Self {
+            mie_k_coefficient: Vec4::new(0.686, 0.678, 0.666, 0.0),
+            primaries: Vec4::new(6.8e-7, 5.5e-7, 4.5e-7, 0.0),
+            turbidity: 12.575,
+            rayleigh: 5.75,
+            mie_coefficient: 0.0074,
+            mie_directional_g: 0.468,
+            luminance: 1.00,
+            refractive_index: 1.000128,
+            num_molecules: 2.542e25,
+            depolarization_factor: 0.137,
+            rayleigh_zenith_length: 3795.0,
+            mie_v: 4.007,
+            mie_zenith_length: 7100.0,
+            sun_intensity_factor: 1024.0,
+            sun_intensity_falloff_steepness: 1.4,
+            sun_angular_diameter_degrees: 0.006,
+            tonemap_weighting: 9.50,
+            update_sun_position,
+            ..Default::default()
+        }
+    }
+
+    pub fn blue_dusk(update_sun_position: bool) -> Self {
+        Self {
+            mie_k_coefficient: Vec4::new(0.686, 0.678, 0.666, 0.0),
+            primaries: Vec4::new(6.8e-7, 5.5e-7, 4.5e-7, 0.0),
+            turbidity: 2.5,
+            rayleigh: 2.295,
+            mie_coefficient: 0.011475,
+            mie_directional_g: 0.814,
+            luminance: 1.00,
+            refractive_index: 1.000262,
+            num_molecules: 2.542e25,
+            depolarization_factor: 0.095,
+            rayleigh_zenith_length: 540.0,
+            mie_v: 3.979,
+            mie_zenith_length: 1000.0,
+            sun_intensity_factor: 1151.0,
+            sun_intensity_falloff_steepness: 1.22,
+            sun_angular_diameter_degrees: 0.00639,
+            tonemap_weighting: 9.50,
+            update_sun_position,
+            ..Default::default()
+        }
+    }
+
+    pub fn purple_dusk(update_sun_position: bool) -> Self {
+        Self {
+            mie_k_coefficient: Vec4::new(0.686, 0.678, 0.666, 0.0),
+            primaries: Vec4::new(7.5e-7, 4.5e-7, 5.1e-7, 0.0),
+            turbidity: 3.6,
+            rayleigh: 2.26,
+            mie_coefficient: 0.005,
+            mie_directional_g: 0.822,
+            luminance: 1.00,
+            refractive_index: 1.000294,
+            num_molecules: 2.542e25,
+            depolarization_factor: 0.068,
+            rayleigh_zenith_length: 12045.0,
+            mie_v: 3.976,
+            mie_zenith_length: 34000.0,
+            sun_intensity_factor: 1631.0,
+            sun_intensity_falloff_steepness: 1.5,
+            sun_angular_diameter_degrees: 0.00933,
+            tonemap_weighting: 9.50,
+            update_sun_position,
+            ..Default::default()
+        }
+    }
+
+    pub fn blood_sky(update_sun_position: bool) -> Self {
+        Self {
+            mie_k_coefficient: Vec4::new(0.686, 0.678, 0.666, 0.0),
+            primaries: Vec4::new(7.929e-7, 3.766e-7, 3.172e-7, 0.0),
+            turbidity: 4.75,
+            rayleigh: 6.77,
+            mie_coefficient: 0.0191,
+            mie_directional_g: 0.793,
+            luminance: 1.1735,
+            refractive_index: 1.000633,
+            num_molecules: 2.542e25,
+            depolarization_factor: 0.01,
+            rayleigh_zenith_length: 1425.0,
+            mie_v: 4.042,
+            mie_zenith_length: 1600.0,
+            sun_intensity_factor: 2069.0,
+            sun_intensity_falloff_steepness: 2.26,
+            sun_angular_diameter_degrees: 0.01487,
+            tonemap_weighting: 9.50,
+            update_sun_position,
+            ..Default::default()
+        }
+    }
 }
 
 pub struct PhysicalSkyCameraTag;
